75ef08f910d5cf6072f09f58dd020e4bb7987c38
Muhammad Saad Malik <saadmalik5211@gmail.com>
2026-02-21 12:59:29 +0500

    added New file for netlify

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -32,13 +32,25 @@ const Hero = () => {
     <section id="home" className="hero" data-scroll-section>
 
       <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 0 }}>
-        <LightRays 
-          raysOrigin="top-center"
-          raysColor="#ffffff"
-          raysSpeed={1}
-          lightSpread={0.5}
-          rayLength={3}
-        />
+       import LightRays from './LightRays';
+
+<div style={{ width: '100%', height: '600px', position: 'relative' }}>
+  <LightRays
+    raysOrigin="top-center"
+    raysColor="#f7f3f3"
+    raysSpeed={1.1}
+    lightSpread={0.5}
+    rayLength={1.5}
+    followMouse={true}
+    mouseInfluence={0.4}
+    noiseAmount={0}
+    distortion={0}
+    className="custom-rays"
+    pulsating={false}
+    fadeDistance={2}
+    saturation={2}
+/>
+</div>
       </div>
 
       <div className="container">
